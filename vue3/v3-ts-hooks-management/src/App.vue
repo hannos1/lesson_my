b3689dd6b330a1aca2cf6a72b73e17a7387b0b28
hannos1 <1972105453@qq.com>
2023-04-07 21:05:32 +0800

    v3ts_admin

---
+++ vue3/v3-ts-hooks-management/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <el-config-provider :locale="zhCn">
+      <router-view />
+  </el-config-provider>
+</template>
+
+<script setup lang="ts">
+// el是全球视野框架
+// elconfigprovider 用于依赖注入
+import { ElConfigProvider } from 'element-plus';
+import zhCn from 'element-plus/es/locale/lang/zh-cn';
+// import en from 'element-plus/es/locale/lang/ja';
+
+
+</script>
+
+<style>
+@import './assets/css/main.css';
+@import './assets/css/color-dark.css';
+
+
+</style>
---
+++ vue3/v3-ts-hooks-management/vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import VueSetupExtend from 'vite-plugin-vue-setup-extend';
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
+// 按需加载的话会 热更新 重新编译 开发时候的打包树和上线后的更新大小
+// element-plus 应该单独打包 单独编译 组件被缓存
+export default defineConfig({
+	base: './',
+	plugins: [
+		vue(),
+		VueSetupExtend(),
+		AutoImport({
+			resolvers: [ElementPlusResolver()]
+		}),
+		Components({
+			resolvers: [ElementPlusResolver()]
+		})
+	]
+});
