2c828076d27192dfe672a218be7efb52426592a5
hannos1 <1972105453@qq.com>
2023-04-16 15:59:07 +0800

    test

---
+++ article_my/vuepress/docs/.vuepress/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    description: 'vue reactive ref VDOM diff',
+    markdown: {
+        lineNumbers: true,  // 编辑器是否显示数字
+    },
+    description:'vue VDOM reactive ref',
+    base:'/',
+    head: [
+        ['link', { rel: 'icon', href: '/logo.png' }]
+    ],
+    title: 'vue_study',
+    port:8581,
+}
