d5d42f0641f886a6960d30b460677fc32bb86da6
hannos1 <1972105453@qq.com>
2023-04-24 21:26:31 +0800

    bloginit

---
+++ node/blog-node-ts/readme.md
@@ -0,0 +1,14 @@
+- ts -> js才能运行
+    typescript tsc
+    tsc-watch  热更新 
+    tsconfig.json  exclude target 
+    @types/express 类型声明文件
+
+- 新的架构 
+- dotenv 解决安全问题
+    - 敏感信息放在.env中
+    - dotenv.config()
+        node操作系统
+    - process.env 解构出port
+    - .gitignore .env不上传
+    - 在信的过的同事间共享
---
+++ node/blog-node-ts/src/app/app.config.ts
@@ -0,0 +1,28 @@
+import dotenv from 'dotenv';  // 私密数据保险箱
+
+dotenv.config()  // 去根目录下找到.env执行 并且添加到process.env对象中
+
+/**
+ * 应用配置
+ * process 进程对象 程序分配资源的最小单元
+ * thread 线程 程序运行的最小单元
+ * GPU 显卡 更适合并行计算  css transform 3D  canvas 3D
+ * CPU 适合线性计算  
+ * process.env 环境变量
+ * 不装@types/node的话默认作为前端js静态编译 不支持process
+ */
+console.log(process.env,'/////')
+export const {APP_PORT} = process.env
+
+/**
+ * 数据库配置
+ */
+export const {
+    MYSQL_HOST,
+    MYSQL_PORT,
+    MYSQL_USER,
+    MYSQL_PASSWORD,
+    MYSQL_DATABASE
+} = process.env
+
+export const {ALLOW_ORIGIN} = process.env
